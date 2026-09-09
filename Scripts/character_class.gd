e8c08bbe83a75fff9513294bcabf4f730f4dcba5
Nikolay Ivanov <wow_niko@protonmail.com>
2026-09-09 23:54:01 +0300

    test

---
+++ Scripts/character_class.gd
@@ -0,0 +1,15 @@
+class_name CharacterClass
+extends Resource
+## What a unit is. Skill trees and starting abilities will hang off this.
+
+@export var display_name: String = "Adventurer"
+@export_multiline var description: String = ""
+@export var icon_color: Color = Color(0.7, 0.7, 0.7)
+
+@export_group("Starting bonuses")
+@export var bonus_strength: int = 0
+@export var bonus_magic_power: int = 0
+@export var bonus_mana_pool: int = 0
+
+## Filled in when abilities exist.
+@export var abilities: Array[Resource] = []
